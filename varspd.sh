ea4672ea3c12ae45acac014c83fe6571ebfbe4af
= <vpatalano@northrockland.org>
2023-03-14 13:41:14 -0400

    commit by make

--- varspd.sh
+++ varspd.sh
@@ -9,10 +9,9 @@ spd=$2
 #output
 outf=$3
 
-echo "--strset3=$sndf" >> .csound6rc
 cat f0z.sco > temp.sco
 echo "i2106 0 0 3 $spd 0" >> temp.sco
 
-csound -W -o$outf main.orc temp.sco
+csound -W -o$outf --strset3=$sndf main.orc temp.sco
 
 rm temp.sco
